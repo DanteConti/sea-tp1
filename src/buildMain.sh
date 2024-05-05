194d6d36b0315b3bbc24e5b07c00ed13a531181c
danteNb <dantejoseconti@gmail.com>
2024-05-05 01:17:10 -0300

    - agregado rootfs.tar.gz - modificado buildMain.sh

--- src/buildMain.sh
+++ src/buildMain.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 incDir="$(pwd)/inc"
+ofname="tp1main.o"
 
-${CROSS_COMPILE}gcc -static main.c -I${incDir} ${incDir}/**.c -o ../bin/main.o
+${CROSS_COMPILE}gcc -static main.c -I${incDir} ${incDir}/**.c -o ../bin/${ofname}
 
 if [ -n "$1" ]; then
- cp ../bin/main.o $1
+ cp ../bin/${ofname} $1
 fi
